ceccf026e63c9aed2faa75fa83a9e75031066bae
Murtaja Ahmed <murtaja.self@gmail.com>
2022-02-24 09:40:18 +0600

    3.comments.php added

---
+++ 3.comment.php
@@ -0,0 +1,17 @@
+<html>
+
+<body>
+
+    <?php
+    // This is single line commented text, and
+    # This is another single line commented text
+
+    /*
+        This is
+        a Multi-lines comment
+        block area
+    */
+    ?>
+</body>
+
+</html>
